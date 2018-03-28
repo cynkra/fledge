a9d31cc75e2bd9e174def123810648def7213b7a
Kirill Müller <krlmlr@mailbox.org>
2018-03-28 23:54:37 +0200

    add date

--- R/update-version.R
+++ R/update-version.R
@@ -2,6 +2,9 @@ update_version_impl <- function(which) {
   desc <- desc::desc(file = "DESCRIPTION")
   desc$bump_version(which)
   new_version <- desc$get_version()
-  add_to_news(paste0("# ", desc$get("Package"), " ", new_version, "\n"))
+  add_to_news(paste0(
+    "# ", desc$get("Package"), " ", new_version,
+    " (", Sys.Date(), ")\n"
+  ))
   desc$write()
 }
