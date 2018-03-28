03ac4254c655bb335897c3ca0f9bbfa8734605a9
Kirill Müller <krlmlr@mailbox.org>
2018-03-29 00:10:49 +0200

    re-tag and re-commit, verbose

--- R/tag-version.R
+++ R/tag-version.R
@@ -5,16 +5,24 @@ tag_version_impl <- function() {
   desc <- desc::desc(file = "DESCRIPTION")
   version <- desc$get_version()
 
-  git2r::add(".", c("DESCRIPTION", "NEWS.md"))
-  if (length(git2r::status(unstaged = FALSE, untracked = FALSE)$staged) > 0) {
-    git2r::commit(".", paste0("Bump version to ", version))
-  }
-
   tag <- paste0("v", version)
   if (tag %in% names(git2r::tags())) {
+    message("Deleting tag ", tag)
     git2r::tag_delete(".", tag)
+
+    if (grepl("^fledge: Bump version to ", git2r::last_commit()$message)) {
+      message("Resetting to previous commit")
+      git2r::reset(git2r::revparse_single(revision = "HEAD^"))
+    }
   }
 
+  git2r::add(".", c("DESCRIPTION", "NEWS.md"))
+  if (length(git2r::status(unstaged = FALSE, untracked = FALSE)$staged) > 0) {
+    message("Committing changes")
+    git2r::commit(".", paste0("fledge: Bump version to ", version))
+  }
+
+  message("Creating tag ", tag)
   msg_header <- paste0(desc$get("Package"), " ", version)
   git2r::tag(".", tag, message = paste0(msg_header, "\n\n", current_news))
 }
