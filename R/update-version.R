fe3ffe61a7a2e41875175c71f34b7d2ee9f705ec
Kirill Müller <krlmlr@mailbox.org>
2018-03-29 15:16:36 +0200

    update Date field if exists
    - If the `DESCRIPTION` has a `"Date"` field, it is populated with the current date in `update_version()`.

--- R/update-version.R
+++ R/update-version.R
@@ -1,5 +1,10 @@
 update_version_impl <- function(which) {
   desc <- desc::desc(file = "DESCRIPTION")
+
+  if (desc$has_fields("Date")) {
+    desc$set("Date", Sys.Date())
+  }
+
   desc$bump_version(which)
   new_version <- desc$get_version()
   add_to_news(paste0(
