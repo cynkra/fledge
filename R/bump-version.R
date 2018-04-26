7280204627cb44416e8e9c4a3b158b014a7ae4c0
Kirill Müller <krlmlr+r@mailbox.org>
2018-04-26 22:56:13 +0200

    don't auto-tag non-dev releases, closes #4
    - Only `"dev"` releases are tagged automatically. For other releases (which usually go to CRAN), it is recommended to postpone tagging until the version is accepted on CRAN (#4).

--- R/bump-version.R
+++ R/bump-version.R
@@ -6,6 +6,12 @@ bump_version_impl <- function(which) {
   update_news()
   #' 2. [update_version()], using the `which` argument
   update_version(which = which)
-  #' 3. [finalize_version()]
-  finalize_version()
+  #' 3. Depending on the `which` argument:
+  if (which == "dev") {
+  #'     - If `"dev"`, [finalize_version()]
+    finalize_version()
+  } else {
+  #'     - Otherwise, [commit_version()].
+    commit_version()
+  }
 }
