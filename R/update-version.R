6bff73224df891810ade15cd80f1047317e3f87e
Kirill Müller <krlmlr+r@mailbox.org>
2018-05-25 11:17:10 +0200

    - Don't write date in NEWS header: It can be found out with "blame" and conflicts with the rendering in pkgdown.

--- R/update-version.R
+++ R/update-version.R
@@ -8,8 +8,7 @@ update_version_impl <- function(which) {
   desc$bump_version(which)
   new_version <- desc$get_version()
   add_to_news(paste0(
-    "# ", desc$get("Package"), " ", new_version,
-    " (", Sys.Date(), ")\n"
+    "# ", desc$get("Package"), " ", new_version, "\n"
   ))
   desc$write()
 }
