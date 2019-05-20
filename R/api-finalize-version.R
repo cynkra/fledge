ce4ad5df4efb89947019a125b46921cfc793d447
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-20 15:07:03 +0200

    Tweak

--- R/api-finalize-version.R
+++ R/api-finalize-version.R
@@ -6,5 +6,5 @@
 #' @export
 finalize_version <- function() {
   finalize_version_impl()
-  ui_todo("If you edit {ui_path('NEWS.md')}, save the file and call {ui_code('fledge::finalize_version()')}")
+  ui_todo("If you edit {ui_path('NEWS.md')} again, save the file and call {ui_code('fledge::finalize_version()')}")
 }
