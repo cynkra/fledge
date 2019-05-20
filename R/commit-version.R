7c7111a18f2c7175cb828ddbd9d1034f4710178e
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-20 14:04:09 +0200

    Extract is_last_commit_bump()

--- R/commit-version.R
+++ R/commit-version.R
@@ -1,10 +1,7 @@
 commit_version_impl <- function() {
   stopifnot(length(git2r::status(".", unstaged = FALSE, untracked = FALSE)$staged) == 0)
 
-  desc <- desc::desc(file = "DESCRIPTION")
-  version <- desc$get_version()
-
-  if (git2r::last_commit()$message == get_commit_message(version)) {
+  if (is_last_commit_bump()) {
     message("Resetting to previous commit")
     git2r::reset(git2r::revparse_single(revision = "HEAD^"))
     amending <- TRUE
@@ -15,12 +12,19 @@ commit_version_impl <- function() {
   git2r::add(".", c("DESCRIPTION", "NEWS.md"))
   if (length(git2r::status(".", unstaged = FALSE, untracked = FALSE)$staged) > 0) {
     message("Committing changes")
-    git2r::commit(".", get_commit_message(version))
+    git2r::commit(".", get_commit_message())
   }
 
   amending
 }
 
+is_last_commit_bump <- function() {
+  git2r::last_commit()$message == get_commit_message()
+}
+
 get_commit_message <- function(version) {
+  desc <- desc::desc(file = "DESCRIPTION")
+  version <- desc$get_version()
+
   paste0("Bump version to ", version)
 }
