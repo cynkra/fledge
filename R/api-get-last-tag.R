428203c466b672b4905fc3703e4f289251b62f78
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-20 14:42:52 +0200

    Document

--- R/api-get-last-tag.R
+++ R/api-get-last-tag.R
@@ -1,4 +1,8 @@
-#' TBD
+#' The most recent tag
+#'
+#' Returns the most recent Git tag.
+#'
+#' @return An element from this repo's [git2r::tags()].
 #'
 #' @export
 get_last_tag <- function() {
--- R/api-get-top-level-commits.R
+++ R/api-get-top-level-commits.R
@@ -1,6 +1,11 @@
-#' TBD
+#' All top-level commits
 #'
-#' @param since since
+#' Return all top-level commits since a particular version
+#' as commit objects.
+#'
+#' @return A list of commit objects similarly to [git2r::commits()].
+#'
+#' @param since A commit or tag object, e.g. [get_last_tag()].
 #' @export
 get_top_level_commits <- function(since) {
   get_top_level_commits_impl(since)
