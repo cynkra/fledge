aa2d7661cc8127c7c164ff1cae19ad33147b8f72
Kirill Müller <krlmlr+r@mailbox.org>
2019-12-28 15:16:25 +0100

    Tweak CI

--- tic.R
+++ tic.R
@@ -1,7 +1,7 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-if (ci_on_travis()) {
+if (ci_on_travis() && ci_has_env("TIC_BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   do_pkgdown()
 }
