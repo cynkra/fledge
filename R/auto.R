1bf4a979a11c993952c9e12e66432fbf904979e8
Kirill Müller <krlmlr+r@mailbox.org>
2020-05-05 06:10:54 +0200

    confirm_submission()

--- R/auto.R
+++ R/auto.R
@@ -13,3 +13,17 @@ get_two_news_headers <- function() {
   top_level_headers <- utils::head(top_level_headers, 2)
   gsub("^#[^0-9]+", "", news[top_level_headers])
 }
+
+
+confirm_submission <- function(url) {
+  parsed <- httr::parse_url(url)
+
+  parsed$query$policy_check2 <- "on"
+  parsed$query$policy_check3 <- "on"
+  parsed$query$policy_check4 <- "on"
+  parsed$query$confirm_submit <- "Upload Package to CRAN"
+
+  url <- httr::build_url(parsed)
+  rlang::inform(glue::glue("Visiting {url}"))
+  utils::browseURL(url)
+}
