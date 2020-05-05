d0c243136e0c286ee85cf40f5e36ef17aa6ef57a
Kirill Müller <krlmlr+r@mailbox.org>
2020-05-05 06:16:02 +0200

    Tweak

--- R/auto.R
+++ R/auto.R
@@ -1,17 +1,18 @@
 is_news_consistent <- function() {
-  last_two <- with_repo(get_two_news_headers())
-  if (length(last_two) < 2) return(TRUE)
+  headers <- with_repo(get_news_headers())
 
-  length(unclass(package_version(last_two[[2]]))[[1]]) <= 3
+  versions <- package_version(headers, strict = TRUE)
+
+  all(lengths(unclass(versions)) <= 3)
 }
 
 
-get_two_news_headers <- function() {
+get_news_headers <- function() {
   news_path <- "NEWS.md"
   news <- readLines(news_path)
   top_level_headers <- grep("^# [a-zA-Z][a-zA-Z0-9.]+[a-zA-Z0-9] [0-9.-]+", news)
   top_level_headers <- utils::head(top_level_headers, 2)
-  gsub("^#[^0-9]+", "", news[top_level_headers])
+  gsub("^# [^0-9]+", "", news[top_level_headers])
 }
 
 
