d900341e75f27d8906c53d300cd1ee83b6b44636
Kirill Müller <krlmlr+r@mailbox.org>
2020-05-10 10:54:43 +0200

    codoc

--- R/api-finalize-version.R
+++ R/api-finalize-version.R
@@ -3,6 +3,7 @@
 #' @description
 #' Calls the following functions:
 #'
+#' @param push If `TRUE`, push the created tag.
 #' @export
 finalize_version <- function(push = FALSE) {
   finalize_version_impl(push)
