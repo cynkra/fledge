7c485a9136af91133b50b8402fc7bc3c2047dc0f
Kirill Müller <krlmlr+r@mailbox.org>
2020-05-10 11:57:23 +0200

    - `finalize_version()` sends `finalize_version(push = TRUE)` to the RStudio console.

--- R/finalize-version.R
+++ R/finalize-version.R
@@ -13,10 +13,18 @@ finalize_version_impl <- function(push) {
   } else {
     ui_todo("Review {ui_path('NEWS.md')}")
     ui_todo("Call {ui_code('fledge::finalize_version(push = TRUE)')}")
+    send_to_console("fledge::finalize_version(push = TRUE)")
   }
 }
 
 push_tag <- function(tag) {
   ui_done("Force-pushing tag {ui_value(tag)}")
   git2r::push(name = "origin", refspec = paste0("refs/tags/", tag), force = TRUE)
+}
+
+send_to_console <- function(code) {
+  if (!is_installed("rstudioapi")) return()
+  if (!is_interactive()) return()
+
+  rstudioapi::sendToConsole(code, execute = FALSE)
 }
