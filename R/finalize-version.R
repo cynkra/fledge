7cdb9be5a4006e640a0f8e81e1fa9ca0ee8c20fb
Kirill Müller <krlmlr+r@mailbox.org>
2020-05-10 10:53:46 +0200

    Don't export

--- R/finalize-version.R
+++ R/finalize-version.R
@@ -7,7 +7,7 @@ finalize_version_impl <- function(push) {
   #' 1. [tag_version()], setting `force = TRUE` if and only if `commit_version()`
   #'   amended a commit.
   tag <- tag_version(force)
-  #' 1. [push_tag()] with the created tag, if `push = TRUE`.
+  #' 1. Force-pushes the created tag to the `"origin"` remote, if `push = TRUE`.
   if (push) {
     push_tag(tag)
   } else {
