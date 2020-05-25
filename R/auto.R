1bcc2f589b79ef1586486cda885679dfe5eb4e42
Kirill Müller <krlmlr+r@mailbox.org>
2020-05-25 06:56:27 +0200

    Allow two-letter package names

--- R/auto.R
+++ R/auto.R
@@ -111,7 +111,7 @@ is_news_consistent <- function() {
 get_news_headers <- function() {
   news_path <- "NEWS.md"
   news <- readLines(news_path)
-  top_level_headers <- grep("^# [a-zA-Z][a-zA-Z0-9.]+[a-zA-Z0-9] [0-9.-]+", news)
+  top_level_headers <- grep("^# [a-zA-Z][a-zA-Z0-9.]*[a-zA-Z0-9] [0-9.-]+", news)
   top_level_headers <- utils::head(top_level_headers, 2)
   gsub("^# [^0-9]+", "", news[top_level_headers])
 }
