c14e8de81b761781094eb08e5afcc7ddc425f48f
pat-s <patrick.schratz@gmail.com>
2020-08-07 14:13:11 +0200

    add github helpers

---
+++ R/gh-helpers.R
@@ -0,0 +1,40 @@
+github_info <- function(path = usethis::proj_get(),
+                        remote = "origin") {
+  remote_url <- get_remote_url(path, remote)
+  repo <- extract_repo(remote_url)
+  get_repo_data(repo)
+}
+
+get_remote_url <- function(path, remote) {
+  r <- git2r::repository(path, discover = TRUE)
+  remote_names <- git2r::remotes(r)
+  if (!length(remote_names)) {
+    stopc("Failed to lookup git remotes")
+  }
+  remote_name <- remote
+  if (!(remote_name %in% remote_names)) {
+    stopc(sprintf(
+      "No remote named '%s' found in remotes: '%s'.",
+      remote_name, remote_names
+    ))
+  }
+  git2r::remote_url(r, remote_name)
+}
+
+extract_repo <- function(url) {
+  # Borrowed from gh:::github_remote_parse
+  re <- "github[^/:]*[/:]([^/]+)/(.*?)(?:\\.git)?$"
+  m <- regexec(re, url)
+  match <- regmatches(url, m)[[1]]
+
+  if (length(match) == 0) {
+    stopc("Unrecognized repo format: ", url)
+  }
+
+  paste0(match[2], "/", match[3])
+}
+
+get_repo_data <- function(repo) {
+  req <- gh::gh("/repos/:repo", repo = repo)
+  return(req)
+}
