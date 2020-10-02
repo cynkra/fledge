a972ec94357dcbd0a52de77987fb78f62de4b986
pat-s <patrick.schratz@gmail.com>
2020-10-02 15:13:36 +0200

    small fixes

--- R/unleash.R
+++ R/unleash.R
@@ -1,16 +1,13 @@
 # Feature: allow to specify a numeric version in `which`
 unleash <- function(which, force = FALSE) {
 
-  state <- check_release_state()
+  state <- check_release_state(which)
 
   switch(state,
     "pre-release" = pre_release(which, force),
     "ready-to-release" = release(),
     "accepted" = post_release(),
     "submitted" = TRUE,
-    "running-release-checks" = TRUE,
-
-    # FIXME: Improve error handling/messages
-    FALSE = stop("No valid state detected.")
+    "running-release-checks" = TRUE
   )
 }
