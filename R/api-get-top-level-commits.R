5f7d4f70411f061dd9b467380d0def846e830a56
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-11 06:45:59 +0100

    Document desired state

--- R/api-get-top-level-commits.R
+++ R/api-get-top-level-commits.R
@@ -3,9 +3,9 @@
 #' Return all top-level commits since a particular version
 #' as commit objects.
 #'
-#' @return A list of commit objects similarly to [git2r::commits()].
+#' @return A character vector of commit SHAs.
 #'
-#' @param since A commit or tag object, e.g. [get_last_tag()].
+#' @param since A commit SHA, e.g. as returned from [get_last_tag()].
 #' @export
 get_top_level_commits <- function(since) {
   with_repo(get_top_level_commits_impl(since))
--- R/api-update-news.R
+++ R/api-update-news.R
@@ -2,7 +2,8 @@
 #'
 #' Lists all commits from a range (default: top-level commits since the most
 #' recent tag) and adds bullets from their body to `NEWS.md`.
-#' @param range range
+#' @param range A commit range as character vector of SHAs,
+#'   e.g. as returned from [get_top_level_commits()].
 #' @export
 update_news <- function(range = get_top_level_commits(since = get_last_tag())) {
   with_repo(update_news_impl(range))
