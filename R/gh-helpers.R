361e01129822ddb9143dfc2059cccc49351ad978
pat-s <patrick.schratz@gmail.com>
2021-04-12 09:15:11 +0200

    stopc -> stop

--- R/gh-helpers.R
+++ R/gh-helpers.R
@@ -9,11 +9,11 @@ get_remote_url <- function(path, remote) {
   r <- git2r::repository(path, discover = TRUE)
   remote_names <- git2r::remotes(r)
   if (!length(remote_names)) {
-    stopc("Failed to lookup git remotes")
+    stop("Failed to lookup git remotes")
   }
   remote_name <- remote
   if (!(remote_name %in% remote_names)) {
-    stopc(sprintf(
+    stop(sprintf(
       "No remote named '%s' found in remotes: '%s'.",
       remote_name, remote_names
     ))
@@ -28,7 +28,7 @@ extract_repo <- function(url) {
   match <- regmatches(url, m)[[1]]
 
   if (length(match) == 0) {
-    stopc("Unrecognized repo format: ", url)
+    stop("Unrecognized repo format: ", url)
   }
 
   paste0(match[2], "/", match[3])
