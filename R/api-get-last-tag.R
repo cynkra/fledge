fe92986a11ab8fb3c92fd0a596860a2e61d1e823
Kirill Müller <krlmlr+r@mailbox.org>
2021-08-31 07:15:49 +0200

    - `get_last_tag()` gains an explanation regarding annotated tags.

--- R/api-get-last-tag.R
+++ R/api-get-last-tag.R
@@ -3,6 +3,9 @@
 #' Returns the most recent Git tag.
 #'
 #' @return The SHA of the commit representing the last tag.
+#'   For annotated tags (as created by fledge), the SHA may be different
+#'   from the SHA of the commit that this tag points to.
+#'   Use [gert::git_log()] to find the actual commit.
 #'
 #' @export
 get_last_tag <- function() {
