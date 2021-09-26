c4cc09cc0c377f35b26538a587bb4b095c18cf84
Kirill Müller <krlmlr+r@mailbox.org>
2021-09-26 12:14:20 +0200

    Avoid dev dependency for rlang
    Closes #124, CC @maelle.

--- tests/testthat/test-bump_version.R
+++ tests/testthat/test-bump_version.R
@@ -1,4 +1,6 @@
 test_that("bump_version() works", {
+  skip_if_not_installed("rlang", "0.4.11.9001")
+
   tempdir <- withr::local_tempdir(pattern = "fledge")
   repo <- create_demo_project(open = FALSE, dir = tempdir, news = TRUE)
   usethis::with_project(
@@ -6,10 +8,29 @@ test_that("bump_version() works", {
       use_r("bla")
       gert::git_add("R/bla.R")
       gert::git_commit("* Add cool bla.")
-      testthat::expect_snapshot(bump_version())
+      expect_snapshot(bump_version())
     }
   )
-  testthat::expect_snapshot_file(
+  expect_snapshot_file(
+    file.path(repo, "NEWS.md"),
+    compare = compare_file_text
+  )
+})
+
+test_that("bump_version() works (CRAN rlang)", {
+  skip_if(packageVersion("rlang") >= "0.4.11.9001")
+
+  tempdir <- withr::local_tempdir(pattern = "fledge")
+  repo <- create_demo_project(open = FALSE, dir = tempdir, news = TRUE)
+  usethis::with_project(
+    path = repo, {
+      use_r("bla")
+      gert::git_add("R/bla.R")
+      gert::git_commit("* Add cool bla.")
+      expect_snapshot(bump_version())
+    }
+  )
+  expect_snapshot_file(
     file.path(repo, "NEWS.md"),
     compare = compare_file_text
   )
