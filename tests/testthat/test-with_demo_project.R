5d2c0c656a54a04f81f3659a1cccb291099921c2
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-11-12 14:04:31 +0100

    add test

---
+++ tests/testthat/test-with_demo_project.R
@@ -0,0 +1,6 @@
+test_that("with_demo_project errors informatively", {
+  expect_snapshot_error(
+    with_demo_project(1+1, dir = "unexisting-dir"),
+    variant = as.character(packageVersion("rlang"))
+  )
+})
