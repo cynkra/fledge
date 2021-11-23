bf5b09a721781e3d80e00f79c5558b9ea3e74deb
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-11-23 14:57:35 +0100

    use with_demo_project() more

---
+++ man/examples/bump-version.R
@@ -0,0 +1,14 @@
+# Create mock package in a temporary directory.
+# Set open to TRUE if you want to play in the mock package.
+with_demo_project(open = FALSE, code = {
+  # Use functions as if inside the newly created package project.
+  # (Or go and actually run code inside the newly created package project!)
+  # Add a new R file.
+  usethis::use_r("cool-function", open = FALSE)
+  # Pretend we added useful code inside it.
+  # Track the new R file with Git.
+  gert::git_add("R/cool-function.R")
+  gert::git_commit("- Add cool function.")
+  # Bump version with fledge.
+  fledge::bump_version()
+})
--- tests/testthat/test-bump_version.R
+++ tests/testthat/test-bump_version.R
@@ -14,30 +14,22 @@ test_that("bump_version() works", {
 
 test_that("bump_version() errors informatively for forbidden notifications", {
 
-  tempdir <- withr::local_tempdir(pattern = "fledge")
-  repo <- create_demo_project(open = FALSE, dir = tempdir, news = TRUE)
-  usethis::with_project(
-    path = repo, {
+  with_demo_project({
       use_r("bla")
       gert::git_add("R/bla.R")
       gert::git_commit("* Add cool bla.")
       desc::desc_set_dep("bla")
       expect_snapshot_error(bump_version(), variant = rlang_version())
-    }
-  )
+  })
 })
 
 test_that("bump_version() errors informatively for wrong branch", {
 
-  tempdir <- withr::local_tempdir(pattern = "fledge")
-  repo <- create_demo_project(open = FALSE, dir = tempdir, news = TRUE)
-  usethis::with_project(
-    path = repo, {
+  with_demo_project({
       use_r("bla")
       gert::git_add("R/bla.R")
       gert::git_commit("* Add cool bla.")
       gert::git_branch_create("bla")
       expect_snapshot_error(bump_version(), variant = rlang_version())
-    }
-  )
+  })
 })
