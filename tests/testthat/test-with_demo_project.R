0310457a98be08f12c6505936478e8c6b9766ef5
Kirill Müller <krlmlr+r@mailbox.org>
2022-02-17 05:24:13 +0100

    Remove rlang_version()

--- tests/testthat/test-with_demo_project.R
+++ tests/testthat/test-with_demo_project.R
@@ -1,6 +1,6 @@
 test_that("with_demo_project errors informatively", {
+  skip_if_not_installed("rlang", "1.0.1")
   expect_snapshot_error(
-    with_demo_project(1 + 1, dir = "unexisting-dir"),
-    variant = rlang_version()
+    with_demo_project(1 + 1, dir = "unexisting-dir")
   )
 })
