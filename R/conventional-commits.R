bfe79f28999088a2074086d9fc5c712b17fb6822
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-05-12 12:36:40 +0200

    clean

--- R/conventional-commits.R
+++
@@ -1,4 +0,0 @@
-# fix type commits should be translated to PATCH releases.
-# feat type commits should be translated to MINOR releases.
-# Commits with BREAKING CHANGE in the commits, regardless of type,
-# should be translated to MAJOR releases.
