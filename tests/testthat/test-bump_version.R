297919da828db8cd71addec589ffe2480c2a04ea
Kirill Müller <krlmlr+r@mailbox.org>
2022-05-12 13:11:46 +0200

    Tests use local_*()

--- tests/testthat/test-bump_version.R
+++ tests/testthat/test-bump_version.R
@@ -2,55 +2,55 @@ test_that("bump_version() works -- dev", {
   skip_if_not_installed("rlang", "1.0.1")
   skip_if_not_installed("testthat", "3.1.2")
 
-  with_demo_project(quiet = TRUE, {
-    create_remote()
-    use_r("bla")
-    gert::git_add("R/bla.R")
-    gert::git_commit("* Add cool bla.")
-    expect_equal(as.character(desc::desc_get_version()), "0.0.0.9000")
-    expect_snapshot(bump_version(), variant = snapshot_variant("testthat"))
-    expect_equal(as.character(desc::desc_get_version()), "0.0.0.9001")
-    expect_equal(get_last_tag()$name, "v0.0.0.9001")
-    expect_snapshot_file("NEWS.md", compare = compare_file_text)
-  })
+  local_demo_project(quiet = TRUE)
+
+  create_remote()
+  use_r("bla")
+  gert::git_add("R/bla.R")
+  gert::git_commit("* Add cool bla.")
+  expect_equal(as.character(desc::desc_get_version()), "0.0.0.9000")
+  expect_snapshot(bump_version(), variant = snapshot_variant("testthat"))
+  expect_equal(as.character(desc::desc_get_version()), "0.0.0.9001")
+  expect_equal(get_last_tag()$name, "v0.0.0.9001")
+  expect_snapshot_file("NEWS.md", compare = compare_file_text)
 })
 
 test_that("bump_version() works -- not dev", {
   skip_if_not_installed("rlang", "1.0.1")
   skip_if_not_installed("testthat", "3.1.2")
 
-  with_demo_project(quiet = TRUE, {
-    create_remote()
-    use_r("bla")
-    gert::git_add("R/bla.R")
-    gert::git_commit("* Add cool bla.")
-    expect_equal(as.character(desc::desc_get_version()), "0.0.0.9000")
-    expect_snapshot(bump_version(which = "major"), variant = snapshot_variant("testthat"))
-    expect_equal(as.character(desc::desc_get_version()), "1.0.0")
-    expect_snapshot_file("NEWS.md", "NEWS-nondev.md", compare = compare_file_text)
-  })
+  local_demo_project(quiet = TRUE)
+
+  create_remote()
+  use_r("bla")
+  gert::git_add("R/bla.R")
+  gert::git_commit("* Add cool bla.")
+  expect_equal(as.character(desc::desc_get_version()), "0.0.0.9000")
+  expect_snapshot(bump_version(which = "major"), variant = snapshot_variant("testthat"))
+  expect_equal(as.character(desc::desc_get_version()), "1.0.0")
+  expect_snapshot_file("NEWS.md", "NEWS-nondev.md", compare = compare_file_text)
 })
 
 test_that("bump_version() errors informatively for forbidden notifications", {
   skip_if_not_installed("rlang", "1.0.1")
 
-  with_demo_project(quiet = TRUE, {
-    use_r("bla")
-    gert::git_add("R/bla.R")
-    gert::git_commit("* Add cool bla.")
-    desc::desc_set_dep("bla")
-    expect_snapshot_error(bump_version())
-  })
+  local_demo_project(quiet = TRUE)
+
+  use_r("bla")
+  gert::git_add("R/bla.R")
+  gert::git_commit("* Add cool bla.")
+  desc::desc_set_dep("bla")
+  expect_snapshot_error(bump_version())
 })
 
 test_that("bump_version() errors informatively for wrong branch", {
   skip_if_not_installed("rlang", "1.0.1")
 
-  with_demo_project(quiet = TRUE, {
-    use_r("bla")
-    gert::git_add("R/bla.R")
-    gert::git_commit("* Add cool bla.")
-    gert::git_branch_create("bla")
-    expect_snapshot_error(bump_version())
-  })
+  local_demo_project(quiet = TRUE)
+
+  use_r("bla")
+  gert::git_add("R/bla.R")
+  gert::git_commit("* Add cool bla.")
+  gert::git_branch_create("bla")
+  expect_snapshot_error(bump_version())
 })
--- tests/testthat/test-tag_version.R
+++ tests/testthat/test-tag_version.R
@@ -1,27 +1,27 @@
 test_that("tag_version() works", {
-  with_demo_project(quiet = TRUE, {
-    use_r("bla")
-    gert::git_add("R/bla.R")
-    gert::git_commit("* Add cool bla.")
-    expect_snapshot(variant = snapshot_variant("testthat"), {
-      tag_version()
-      get_last_tag()[, c("name", "ref")]
-    })
+  local_demo_project(quiet = TRUE)
 
-    # Attempting tagging again without any new commit
-    expect_silent(shut_up_fledge(tag_version()))
+  use_r("bla")
+  gert::git_add("R/bla.R")
+  gert::git_commit("* Add cool bla.")
+  expect_snapshot(variant = snapshot_variant("testthat"), {
+    tag_version()
+    get_last_tag()[, c("name", "ref")]
+  })
 
-    # Attempting tagging again
-    # with a new commit but same version
-    use_r("pof")
-    gert::git_add("R/pof.R")
-    gert::git_commit("* Add cool pof.")
-    expect_snapshot_error(shut_up_fledge(tag_version(force = FALSE)))
+  # Attempting tagging again without any new commit
+  expect_silent(shut_up_fledge(tag_version()))
 
-    # Same, but forcing
-    expect_snapshot(variant = snapshot_variant("testthat"), {
-      tag_version(force = TRUE)
-      get_last_tag()[, c("name", "ref")]
-    })
+  # Attempting tagging again
+  # with a new commit but same version
+  use_r("pof")
+  gert::git_add("R/pof.R")
+  gert::git_commit("* Add cool pof.")
+  expect_snapshot_error(shut_up_fledge(tag_version(force = FALSE)))
+
+  # Same, but forcing
+  expect_snapshot(variant = snapshot_variant("testthat"), {
+    tag_version(force = TRUE)
+    get_last_tag()[, c("name", "ref")]
   })
 })
