89cc31c03afb4c9735b3e7415b462d248961aa16
Kirill Müller <krlmlr+r@mailbox.org>
2022-05-12 10:21:47 +0200

    Simplify test

--- tests/testthat/test-finalize-version.R
+++ tests/testthat/test-finalize-version.R
@@ -1,8 +1,6 @@
 test_that("finalize_version(push = FALSE)", {
   skip_if_not_installed("rlang", "1.0.1")
 
-  news_tempdir <- withr::local_tempdir(pattern = "news")
-
   with_demo_project(quiet = TRUE, {
     use_r("bla")
     gert::git_add("R/bla.R")
@@ -14,19 +12,13 @@ test_that("finalize_version(push = FALSE)", {
     writeLines(news, "NEWS.md")
 
     expect_snapshot(finalize_version(push = FALSE), variant = snapshot_variant("testthat"))
-    file.copy("NEWS.md", file.path(news_tempdir, "NEWS-push-false.md"))
-  })
 
-  expect_snapshot_file(
-    file.path(news_tempdir, "NEWS-push-false.md"),
-    compare = compare_file_text
-  )
+    expect_snapshot_file("NEWS.md", "NEWS-push-false.md", compare = compare_file_text)
+  })
 })
 
 test_that("finalize_version(push = TRUE)", {
   skip_if_not_installed("rlang", "1.0.1")
-
-  news_tempdir <- withr::local_tempdir(pattern = "news")
 
   with_demo_project(quiet = TRUE, {
     remote_url <- create_remote()
@@ -44,11 +36,7 @@ test_that("finalize_version(push = TRUE)", {
       show_tags(remote_url)
       show_files(remote_url)
     })
-    file.copy("NEWS.md", file.path(news_tempdir, "NEWS-push-true.md"))
-  })
 
-  expect_snapshot_file(
-    file.path(news_tempdir, "NEWS-push-true.md"),
-    compare = compare_file_text
-  )
+    expect_snapshot_file("NEWS.md", "NEWS-push-true.md", compare = compare_file_text)
+  })
 })
