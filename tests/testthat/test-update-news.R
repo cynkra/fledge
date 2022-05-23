e0a0d3fd6a696a196ca3568b128da31b7305491f
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-05-23 13:13:53 +0200

    switch for tests

--- tests/testthat/test-update-news.R
+++ tests/testthat/test-update-news.R
@@ -34,6 +34,7 @@ test_that("Can parse conventional commits", {
 })
 
 test_that("Can parse PR merge commits", {
+  withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
   httptest::with_mock_dir("pr", {
     withr::local_envvar("GITHUB_PAT" = "ghp_111111111111111111111111111111111111111")
     expect_snapshot(extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing"))
@@ -41,6 +42,7 @@ test_that("Can parse PR merge commits", {
 })
 
 test_that("Can parse PR merge commits - internet error", {
+  withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
   withr::local_envvar("NO_INTERNET_TEST_FLEDGE" = "blop")
   expect_snapshot(extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing"))
 })
@@ -51,6 +53,7 @@ test_that("Can parse PR merge commits - PAT error", {
 })
 
 test_that("Can parse PR merge commits - other error", {
+  withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
   bla <- function(...) stop("bla")
   mockery::stub(harvest_pr_data, "gh::gh", bla)
   expect_snapshot(harvest_pr_data("Merge pull request #332 from cynkra/conventional-parsing"))
