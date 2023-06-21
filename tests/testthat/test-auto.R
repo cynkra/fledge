f526163d6626a691789045d85fc6ea7058541383
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-21 14:00:40 +0200

    Add version and ref arguments to create_release_branch(), fix create_release_branch(force = TRUE)

--- tests/testthat/test-auto.R
+++ tests/testthat/test-auto.R
@@ -11,3 +11,18 @@ test_that("guess_next_impl() works", {
     guess_next_impl("1.99.99.9009")
   })
 })
+
+test_that("create_release_branch() works", {
+  local_demo_project()
+  gert::git_branch_create("bla")
+  gert::git_branch_checkout("main")
+
+  expect_snapshot({
+    create_release_branch("0.0.1", ref = "bla")
+  })
+
+  gert::git_branch_checkout("main")
+  expect_snapshot(error = TRUE, {
+    create_release_branch("0.0.1", ref = "blop", force = TRUE)
+  })
+})
