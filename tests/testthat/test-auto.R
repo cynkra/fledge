1656c30c5d3f5dbe9465884b765e6c98e0e47504
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-21 14:00:40 +0200

    Add fledgeling and ref arguments to create_release_branch(), fix create_release_branch(force = TRUE)

--- tests/testthat/test-auto.R
+++ tests/testthat/test-auto.R
@@ -11,3 +11,21 @@ test_that("guess_next_impl() works", {
     guess_next_impl("1.99.99.9009")
   })
 })
+
+test_that("create_release_branch() works", {
+  local_demo_project()
+  gert::git_branch_create("bla")
+  gert::git_branch_checkout("main")
+  fledgeling <- read_fledgling()
+  expect_snapshot({
+    create_release_branch(fledgeling, ref = "bla")
+  })
+
+  gert::git_branch_checkout("main")
+  expect_snapshot(
+    {
+      create_release_branch(fledgeling, ref = "blop", force = TRUE)
+    },
+    error = TRUE
+  )
+})
