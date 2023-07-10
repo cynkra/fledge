f652080ec143fea0e5d6edf41f36357a7b2720ab
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-07-10 15:10:30 +0200

    test: init_release()

--- tests/testthat/test-auto.R
+++ tests/testthat/test-auto.R
@@ -49,3 +49,26 @@ test_that("create_release_branch() works", {
     create_release_branch("0.0.1", ref = "blop", force = TRUE)
   })
 })
+
+test_that("init_release() works", {
+  local_options(repos = NULL) # because of usethis::use_news_md() -> available.packages()
+  local_demo_project(quiet = TRUE)
+
+  shut_up_fledge(bump_version())
+  expect_snapshot(init_release())
+  expect_true(gert::git_branch_exists("cran-0.0.1"))
+})
+
+test_that("init_release() -- force", {
+  withr::local_envvar("FLEDGE_TEST_NOGH" = "blop")
+  local_options(repos = NULL) # because of usethis::use_news_md() -> available.packages()
+  local_demo_project(quiet = TRUE)
+
+  shut_up_fledge(bump_version())
+
+  gert::git_branch_create("cran-0.0.1", checkout = FALSE)
+
+  expect_snapshot(init_release(), error = TRUE)
+  expect_snapshot(init_release(force = TRUE))
+  expect_true(gert::git_branch_exists("cran-0.0.1"))
+})
