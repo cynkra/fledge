02af676b0e6461f201272ca0728276ff3666a970
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-02 14:53:59 +0200

    start script

--- inst/marshmallow.R
+++ inst/marshmallow.R
@@ -3,16 +3,7 @@ pkgload::load_all()
 rlang::local_options(rlang_interactive = FALSE)
 
 # initial state ----
-# Avoid cleanup
-project_dir <- tempfile(pattern = "fledge")
-dir.create(project_dir)
-local_demo_project(project_dir, quiet = TRUE)
-# local_demo_project(quiet = TRUE)
-
-term <- rstudioapi::terminalCreate()
-rstudioapi::terminalSend(term, paste0("cd ", getwd(), "\n"))
-
-# create remote ----
+local_demo_project(quiet = TRUE)
 tempdir_remote <- withr::local_tempdir(pattern = "remote")
 create_remote(tempdir_remote)
 
@@ -25,33 +16,12 @@ use_r("bla", open = FALSE)
 gert::git_add("R/bla.R")
 gert::git_commit("* Add cool bla.")
 bump_version()
-finalize_version(push = TRUE)
 
-# prep release ----
+# init release ----
 withr::local_envvar("FLEDGE_TEST_NOGH" = "no-github-no-mocking-needed-yay")
 withr::local_envvar("FLEDGE_DONT_BOTHER_CRAN_THIS_IS_A_TEST" = "yes-a-test")
-pre_release()
-gert::git_branch()
-gert::git_branch_list(local = TRUE)
-desc::desc_get_version()
-gert::git_status()
+init_release()
 
-# check boxes ----
-cran_comments <- get_cran_comments_text()
-writeLines(cran_comments)
-cran_comments <- gsub("- \\[ \\]", "- \\[x\\]", cran_comments)
-brio::write_lines(cran_comments, "cran-comments.md")
-gert::git_add("cran-comments.md")
-gert::git_commit("this is how we check boxes")
-
-# release ----
-withr::local_envvar("FLEDGE_DONT_BOTHER_CRAN_THIS_IS_A_TEST" = "yes-a-test")
-release()
-gert::git_status()
-fs::dir_tree()
-
-# post release ----
-post_release()
 
 # back to fledge directory and project ----
 back_to_start()
