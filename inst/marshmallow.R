696eb2d781a275a303d8c64ba931b3f2293b84e1
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-02 15:05:54 +0200

    more steps

--- inst/marshmallow.R
+++ inst/marshmallow.R
@@ -19,7 +19,19 @@ bump_version()
 
 # init release ----
 init_release()
+gert::git_branch()
+gert::git_branch_list()
+desc::desc_get_version()
+gert::git_status()
+gert::git_diff()$patch[[1]] |> cat()
+gert::git_diff()$patch[[2]] |> cat()
+gert::git_diff()$patch[[3]] |> cat()
 
+# prep release ----
+withr::local_envvar("FLEDGE_TEST_NOGH" = "no-github-no-mocking-needed-yay")
+pre_release()
+
+# release ----
 
 # back to fledge directory and project ----
 back_to_start()
