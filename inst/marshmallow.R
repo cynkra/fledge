728a5d36c2b82f11064cfc5051fd90d2d8c660af
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-02 15:05:54 +0200

    more steps

--- inst/marshmallow.R
+++ inst/marshmallow.R
@@ -21,7 +21,25 @@ bump_version()
 withr::local_envvar("FLEDGE_TEST_NOGH" = "no-github-no-mocking-needed-yay")
 withr::local_envvar("FLEDGE_DONT_BOTHER_CRAN_THIS_IS_A_TEST" = "yes-a-test")
 init_release()
+gert::git_branch()
+gert::git_branch_list()
+desc::desc_get_version()
+gert::git_status()
+gert::git_diff()$patch[[1]] |> cat()
+gert::git_diff()$patch[[2]] |> cat()
+gert::git_diff()$patch[[3]] |> cat()
 
+# prep release ----
+pre_release()
+
+# release ----
+cran_comments <- get_cran_comments_text()
+cran_comments <- gsub("- \\[ \\]", "- \\[x\\]", cran_comments)
+brio::write_lines(cran_comments, "cran-comments.md")
+gert::git_add("cran-comments.md")
+gert::git_commit("this is how we check boxes")
+withr::local_envvar("FLEDGE_DONT_BOTHER_CRAN_THIS_IS_A_TEST" = "yes-a-test")
+release()
 
 # back to fledge directory and project ----
 back_to_start()
