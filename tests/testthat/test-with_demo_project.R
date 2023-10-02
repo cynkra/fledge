0963472e7cba5356f54d23236fe932612fdd1dd7
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-02 12:07:00 +0200

    update test since expect_snapshot_error() is experimental

--- tests/testthat/test-with_demo_project.R
+++ tests/testthat/test-with_demo_project.R
@@ -1,6 +1,5 @@
 test_that("with_demo_project errors informatively", {
-  skip_if_not_installed("rlang", "1.0.1")
-  expect_snapshot_error(
+  expect_snapshot(error = TRUE, {
     with_demo_project(1 + 1, dir = "unexisting-dir")
-  )
+  })
 })
