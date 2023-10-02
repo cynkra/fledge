c057523f6bc6233f93fd5b35c3d83f794cd53ac0
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-02 11:38:17 +0200

    format, rm variant

--- tests/testthat/test-tag_version.R
+++ tests/testthat/test-tag_version.R
@@ -4,23 +4,25 @@ test_that("tag_version() works", {
   use_r("bla")
   gert::git_add("R/bla.R")
   gert::git_commit("* Add cool bla.")
-  expect_snapshot(variant = snapshot_variant("testthat"), {
+
+  expect_snapshot({
     tag_version()
     get_last_tag()[, c("name", "ref")]
   })
 
-  # Attempting tagging again without any new commit
-  expect_silent(shut_up_fledge(tag_version()))
+  ## Attempting tagging again without any new commit ----
+  shut_up_fledge(tag_version())
 
-  # Attempting tagging again
+  ## Attempting tagging again ----
   # with a new commit but same version
   use_r("pof")
   gert::git_add("R/pof.R")
   gert::git_commit("* Add cool pof.")
+
   expect_snapshot_error(shut_up_fledge(tag_version(force = FALSE)))
 
-  # Same, but forcing
-  expect_snapshot(variant = snapshot_variant("testthat"), {
+  ## Same, but forcing ----
+  expect_snapshot({
     tag_version(force = TRUE)
     get_last_tag()[, c("name", "ref")]
   })
