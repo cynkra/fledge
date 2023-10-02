a70cbe8cbd120b878df80843a55144a754097da1
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-02 11:33:26 +0200

    clean test file

--- tests/testthat/test-parse-news-items.R
+++ tests/testthat/test-parse-news-items.R
@@ -31,28 +31,48 @@ test_that("Will use commits", {
   )
 
   update_news(which = "minor")
-  file.copy("NEWS.md", "NEWS-merge.md")
-  expect_snapshot_file("NEWS-merge.md")
+
+  expect_snapshot_file("NEWS.md", "NEWS-merge.md")
 })
 
 test_that("Can parse Co-authored-by", {
-  expect_snapshot(extract_newsworthy_items("- blop\n-blip\n\nCo-authored-by: Person (<person@users.noreply.github.com>)"))
-  expect_snapshot(extract_newsworthy_items("- blop (#42)\n\nCo-authored-by: Person (<person@users.noreply.github.com>)\nCo-authored-by: Someone Else (<else@users.noreply.github.com>)"))
-  expect_snapshot(extract_newsworthy_items("feat: blop (#42)\n\nCo-authored-by: Person (<person@users.noreply.github.com>)"))
+  expect_snapshot(
+    extract_newsworthy_items(
+      "- blop\n-blip\n\nCo-authored-by: Person (<person@users.noreply.github.com>)"
+    )
+  )
+
+  expect_snapshot(
+    extract_newsworthy_items(
+      "- blop (#42)\n\nCo-authored-by: Person (<person@users.noreply.github.com>)\nCo-authored-by: Someone Else (<else@users.noreply.github.com>)"
+    )
+  )
+
+  expect_snapshot(
+    extract_newsworthy_items(
+      "feat: blop (#42)\n\nCo-authored-by: Person (<person@users.noreply.github.com>)"
+    )
+  )
 })
 
 test_that("Can parse PR merge commits", {
   withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
+
   with_mock_dir("pr", {
     withr::local_envvar("YES_INTERNET_TEST_FLEDGE" = "bla")
     withr::local_envvar("FLEDGE_TEST_SCOPES" = "bla")
     withr::local_envvar("GITHUB_PAT" = "ghp_111111111111111111111111111111111111111")
-    expect_snapshot_tibble(extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing"))
+    expect_snapshot_tibble(
+      extract_newsworthy_items(
+        "Merge pull request #332 from cynkra/conventional-parsing"
+        )
+    )
   })
 })
 
 test_that("Can parse PR merge commits - external contributor", {
   withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
+
   with_mock_dir("pr0", {
     withr::local_envvar("YES_INTERNET_TEST_FLEDGE" = "bla")
     withr::local_envvar("FLEDGE_TEST_SCOPES" = "bla")
@@ -66,11 +86,13 @@ test_that("Can parse PR merge commits - external contributor", {
 
 test_that("Can parse PR merge commits - linked issues", {
   withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
+
   with_mock_dir("pr2", {
     withr::local_envvar("YES_INTERNET_TEST_FLEDGE" = "bla")
     withr::local_envvar("FLEDGE_TEST_SCOPES" = "bla")
     withr::local_envvar("GITHUB_PAT" = "ghp_111111111111111111111111111111111111111")
-    expect_snapshot_tibble(extract_newsworthy_items("Merge pull request #328 from cynkra/blop"))
+    expect_snapshot_tibble(
+      extract_newsworthy_items("Merge pull request #328 from cynkra/blop"))
   })
 })
 
@@ -80,7 +102,10 @@ test_that("Can parse PR merge commits - internet error", {
   withr::local_envvar("GITHUB_PAT" = "ghp_111111111111111111111111111111111111111")
   withr::local_envvar("FLEDGE_TEST_GITHUB_SLUG" = "cynkra/fledge")
   withr::local_envvar("NO_INTERNET_TEST_FLEDGE" = "blop")
-  expect_snapshot(extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing"))
+
+  expect_snapshot(
+    extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing")
+  )
 })
 
 test_that("Can parse PR merge commits - PAT absence", {
@@ -88,7 +113,9 @@ test_that("Can parse PR merge commits - PAT absence", {
   withr::local_envvar("FLEDGE_TEST_SCOPES" = "bla")
   withr::local_envvar("GITHUB_PAT" = "ghp_111111111111111111111111111111111111111")
   withr::local_envvar("FLEDGE_TEST_NO_PAT" = "blop")
-  expect_snapshot_error(extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing"))
+  expect_snapshot_error(
+    extract_newsworthy_items("Merge pull request #332 from cynkra/conventional-parsing")
+  )
 })
 
 test_that("Can parse PR merge commits - other error", {
@@ -100,6 +127,8 @@ test_that("Can parse PR merge commits - other error", {
   with_mock_dir("pr", {
     withr::local_envvar("FLEDGE_TEST_SCOPES" = "bla")
     withr::local_envvar("GITHUB_PAT" = "ghp_111111111111111111111111111111111111111")
-    expect_snapshot_tibble(harvest_pr_data("Merge pull request #332 from cynkra/conventional-parsing"))
+    expect_snapshot_tibble(
+      harvest_pr_data("Merge pull request #332 from cynkra/conventional-parsing")
+    )
   })
 })
