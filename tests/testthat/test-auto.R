16cfedf41a1f68977b4fd762b00da3d6ed76ffd6
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-09 13:21:34 +0200

    add test from marshmallow-abandon

--- tests/testthat/test-auto.R
+++ tests/testthat/test-auto.R
@@ -241,3 +241,40 @@ test_that("full cycle pre-minor", {
     c("v0.0.99.9000", "v0.1.0", "v0.1.0.9000")
   )
 })
+
+test_that("release abandon", {
+  # not opening anything ----
+  rlang::local_options(rlang_interactive = FALSE)
+
+  # initial state ----
+  local_demo_project(quiet = TRUE)
+
+  # create remote ----
+  tempdir_remote <- withr::local_tempdir(pattern = "remote")
+  create_remote(tempdir_remote)
+
+  # some edits ----
+  use_r("bla")
+  gert::git_add("R/bla.R")
+  gert::git_commit("* Add cool bla.")
+  shut_up_fledge(bump_version())
+  shut_up_fledge(finalize_version(push = TRUE))
+
+  # init release ----
+  withr::local_envvar("FLEDGE_TEST_NOGH" = "no-github-no-mocking-needed-yay")
+  withr::local_envvar("FLEDGE_DONT_BOTHER_CRAN_THIS_IS_A_TEST" = "yes-a-test")
+  try(init_release())
+
+  expect_snapshot(error = TRUE, {
+    init_release(force = TRUE)
+  })
+
+  gert::git_branch_checkout("main")
+
+  expect_snapshot(error = TRUE, {
+    init_release(force = FALSE)
+  })
+
+  expect_snapshot(init_release(force = TRUE))
+
+})
