80727a35aa3df587ea96f7c599a15fce1af1557f
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-09 12:48:42 +0200

    quotes

--- tests/testthat/helper-cleanup.R
+++ tests/testthat/helper-cleanup.R
@@ -11,6 +11,7 @@ back_to_start <- function() {
 clean_submission_messages <- function(x) {
   x <- x[!grepl("pdflatex not found", x)]
   x[startsWith(x, "* checking for file")] <- "* checking for file DESCRIPTION"
+  x[startsWith(x, "* building")] <- "* building 'tea_0.0.1.tar.gz'"
   x[startsWith(x, "i Submitting file:")] <- "i Submitting file: 'tea_0.0.1.tar.gz'"
   x[startsWith(x, "i File size:")] <- 'i File size: "some hundreds of bytes"'
 
