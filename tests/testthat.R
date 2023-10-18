808705c1e571d48c71178d466614497dbc9982dc
Kirill Müller <kirill@cynkra.com>
2023-10-18 10:19:55 +0200

    Only suggest httptest2

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,12 @@
-deps <- c("testthat", "httptest2")
-deps_availability <- purrr::map_lgl(deps, rlang::is_installed)
-all_deps_available <- all(deps_availability)
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
 
-if (all_deps_available) {
-  library(testthat)
-  library(fledge)
-  test_check("fledge")
-} else {
-  message(
-    sprintf(
-      "%s not available.",
-      toString(deps[!deps_availability])
-    )
-  )
-}
+library(testthat)
+library(fledge)
+
+test_check("fledge")
--- tests/testthat/helper-httptest2.R
+++ tests/testthat/helper-httptest2.R
@@ -1,5 +1,3 @@
-library("httptest2")
-
 with_mock_dir <- function(name, ...) {
   httptest2::with_mock_dir(file.path("../fixtures", name), ...)
 }
