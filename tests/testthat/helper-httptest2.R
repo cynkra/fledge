8ec7d85b51a1adb169ce5df8791220ddbe7f48e6
Kirill Müller <kirill@cynkra.com>
2023-10-18 14:34:47 +0200

    Make sure full test matrix is run

--- tests/testthat/helper-httptest2.R
+++ tests/testthat/helper-httptest2.R
@@ -1,3 +1,4 @@
 with_mock_dir <- function(name, ...) {
+  skip_if_not_installed("httptest2") # Needed for httptest2::with_mock_dir
   httptest2::with_mock_dir(file.path("../fixtures", name), ...)
 }
