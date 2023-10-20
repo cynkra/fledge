21e30efcd5e590cda8848259e2eb8a433442b0f4
Kirill Müller <kirill@cynkra.com>
2023-10-20 11:43:01 +0200

    Revert 2

--- R/utils-gh-pat.R
+++ R/utils-gh-pat.R
@@ -1,8 +1,4 @@
 check_gh_pat <- function(needed_scopes = "repo") {
-  if (nzchar(Sys.getenv("FLEDGE_TEST_NOGH"))) {
-    return(TRUE)
-  }
-
   if (!nzchar(gh::gh_token()) || nzchar(Sys.getenv("FLEDGE_TEST_NO_PAT"))) {
     cli::cli_abort(
       message = c(
