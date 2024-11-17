5e44399302dff0e005aa8de15350d8ffedf6f058
Kirill Müller <kirill@cynkra.com>
2024-11-17 06:34:48 +0100

    has_src

--- R/auto.R
+++ R/auto.R
@@ -137,7 +137,7 @@ plan_release_impl <- function(which, force) {
     }))
     if (has_src) {
       inject(job::empty(title = "rhub", {
-        if (FALSE) rhub::rhub_check(platforms = rhub::rhub_platforms()$name, branch = !!release_branch)
+        if (has_src) rhub::rhub_check(platforms = rhub::rhub_platforms()$name, branch = !!release_branch)
       }))
     }
     inject(job::empty(title = "url_update", {
