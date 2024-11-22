f0a3cc0d60809f4b09ba731c88650ca9b92e3c11
Kirill Müller <krlmlr@users.noreply.github.com>
2024-11-22 20:40:06 +0100

    feat: `finalize_version()` unconditionally overwrites existing tags (#819)

--- R/finalize-version.R
+++ R/finalize-version.R
@@ -3,10 +3,9 @@
 finalize_version_impl <- function(push, suggest_finalize = TRUE) {
   #' @description
   #' 1. [commit_version()]
-  force <- commit_version()
-  #' 1. [tag_version()], setting `force = TRUE` if and only if `commit_version()`
-  #'   amended a commit.
-  tag <- tag_version(force)
+  commit_version()
+  #' 1. [tag_version()] with `force = TRUE`
+  tag <- tag_version(TRUE)
   #' 1. Force-pushes the created tag to the `"origin"` remote, if `push = TRUE`.
   if (push) {
     push_head()
